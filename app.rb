e4b2bac1889172d55c70471f64d7a3da46e9dd95
Joe Letizia <pair+joe@joeletizia.com>
2014-01-05 21:17:57 -0500

    removed carousel and went with a simple display

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
 require 'rubygems'
 require 'sinatra'
-require 'Haml'
+require 'haml'
 
 get '/' do
   haml :index
 end
 
+get '/event_info' do
+  haml :event_info
+end
+
+get '/images' do
+  haml :images
+end
+
+get '/contact' do
+  haml :contact
+end
+
+get '/registry' do
+  haml :registry
+end
